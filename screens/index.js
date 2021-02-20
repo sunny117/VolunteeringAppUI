d28e8209b52a9c33c6d19345e4f108c63750e746
Marella Shiva Sai Teja <shivasai962@gmail.com>
2021-02-20 21:36:58 +0530

    small changes in configuration

--- screens/index.js
+++ screens/index.js
@@ -7,26 +7,26 @@ import Organization from './Organization';
 import { connect } from "react-redux";
 
 class Screens extends React.Component {
-  constructor(props){
-    super(props);
-  };
+    constructor(props) {
+        super(props);
+    };
 
-  render() {
-    if(this.props.isSignedIn)
-      return this.props.userType === 'volunteer' ? <Volunteer/> : <Organization/>;
-    return <SignIn/>;
-  };
+    render() {
+        if(this.props.isSignedIn)
+            return this.props.userType === 'volunteer' ? <Volunteer /> : <Organization />;
+        return <SignIn />;
+    };
 };
 
 function mapStateToProps(state) {
-  return {
-      isSignedIn: state.authReducer.isSignedIn,
-      userType: state.authReducer.userType
-  };
+    return {
+        isSignedIn: state.authReducer.isSignedIn,
+        userType: state.authReducer.userType
+    };
 };
 
 function mapDispatchToProps(dispatch) {
-  return {};
+    return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Screens);
