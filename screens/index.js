6d87fe7dd8cacce06b5fbb5c44509cd9919c1cf4
Marella Shiva Sai Teja <shivasai962@gmail.com>
2021-02-26 11:18:56 +0530

    Added signIn silently functionality

--- screens/index.js
+++ screens/index.js
@@ -3,16 +3,39 @@ import React from 'react';
 import SignIn from './SignIn';
 import Volunteer from './Volunteer';
 import Organization from './Organization';
+import GoogleApi from '../util/googleApi'
 
 import { connect } from "react-redux";
+import { bindActionCreators } from 'redux';
+import * as authActions from '../store/Actions/authActions';
 
 class Screens extends React.Component {
     constructor(props) {
         super(props);
     };
 
+    componentDidMount() {
+        GoogleApi.isSignedIn()
+            .then(isSignedInValue => {
+                if (isSignedInValue) {
+                    GoogleApi.signInSilently()
+                        .then(result => {
+                            let authState = {
+                                isSignedIn: true,
+                                userType: 'volunteer',
+                                userName: result.user.name,
+                                userPhotoUrl: result.user.photo,
+                                userEmail: result.user.email,
+                            };
+                            this.props.authActions.setAuth(authState);
+                        })
+                        .catch((error) => console.log(error));
+                }
+            })
+    }
+
     render() {
-        if(this.props.isSignedIn)
+        if (this.props.isSignedIn)
             return this.props.userType === 'volunteer' ? <Volunteer /> : <Organization />;
         return <SignIn />;
     };
@@ -26,7 +49,9 @@ function mapStateToProps(state) {
 };
 
 function mapDispatchToProps(dispatch) {
-    return {};
+    return {
+        authActions: bindActionCreators(authActions, dispatch),
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Screens);
