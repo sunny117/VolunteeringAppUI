7c2415d7b18bd327d155f8e362d2586d265a7f3a
Marella Shiva Sai Teja <shivasai962@gmail.com>
2021-03-11 21:15:55 +0530

    Added initial version of input form

--- screens/Organization/Home/index.js
+++ screens/Organization/Home/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, TouchableOpacity, Text, Image, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import { View, TouchableOpacity, Text, Image, StyleSheet, TouchableWithoutFeedback, TextInput } from 'react-native';
 
 import { connect } from "react-redux";
 import { bindActionCreators } from 'redux';
 
 import Location from 'app/components/Location.js'
+import Date from 'app/components/Date';
 
 class Home extends React.Component {
 	constructor(props) {
@@ -14,17 +15,34 @@ class Home extends React.Component {
 	render() {
 		return (
 			<TouchableWithoutFeedback>
-				<Location/>
+				<View style={{ flex: 1 }}>
+					<View>
+						<TextInput 
+							defaultValue= ""
+							maxLength= {64}
+							placeholder= "Heading"
+							placeholderTextColor= "#a6a6a6"
+						/>
+					</View>
+					<View>
+						<TextInput 
+							defaultValue= ''
+							multiline
+							maxLength={2048}
+							placeholder= "Description"
+							placeholderTextColor= "#a6a6a6"
+						/>
+					</View>
+					<Location />
+					<Date />
+					<Date />
+				</View>
 			</TouchableWithoutFeedback>
 		);
 	};
 };
 
-const styles = StyleSheet.create({
-	mainContainer: {
-		display: 'flex'
-	}
-});
+const styles = StyleSheet.create({});
 
 function mapStateToProps(state) {
 	return {
