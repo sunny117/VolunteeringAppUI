f372b80f4091b53a00ed985069fa146211ae1ec2
Marella Shiva Sai Teja <shivasai962@gmail.com>
2021-03-11 21:14:06 +0530

    Added actions to manipulate state of activity

---
+++ store/Actions/activity.js
@@ -0,0 +1,43 @@
+export function setHeading( heading ) {
+	return {
+		type: 'SET_HEADING',
+		heading
+	}
+}
+
+export function setType( value ) {
+	return {
+		type: 'SET_TYPE',
+		value
+	}
+}
+
+export function setLocation( address, longitude, latitude ) {
+	return {
+		type: 'SET_LOCATION',
+		address,
+		longitude,
+		latitude
+	}
+}
+
+export function setStartDate( startDate ) {
+	return {
+		type: 'SET_START_DATE',
+		startDate
+	}
+}
+
+export function setEndDate( endDate ) {
+	return {
+		type: 'SET_END_DATE',
+		endDate
+	}
+}
+
+export function setDescription( description ) {
+	return {
+		type: 'SET_DESCRIPTION',
+		description
+	}
+}
