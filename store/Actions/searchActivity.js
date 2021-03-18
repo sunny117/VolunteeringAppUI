9d863e81f5ec3d80ad4cef1fafef271f047c8213
Marella Shiva Sai Teja <shivasai962@gmail.com>
2021-03-18 14:55:08 +0530

    Added store for searching activities

---
+++ store/Actions/searchActivity.js
@@ -0,0 +1,29 @@
+export function setType( value ) {
+	return {
+		type: 'SET_TYPE',
+		value
+	}
+}
+
+export function setLocation( address, longitude, latitude ) {
+	return {
+		type: 'SET_LOCATION',
+		address,
+		longitude,
+		latitude
+	}
+}
+
+export function setStartDate( startDate ) {
+	return {
+		type: 'SET_START_DATE',
+		startDate
+	}
+}
+
+export function setEndDate( endDate ) {
+	return {
+		type: 'SET_END_DATE',
+		endDate
+	}
+}
