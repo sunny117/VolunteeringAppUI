f915241a45a69e0e8de489b7e0c1c5cb2536533d
Srikar Reddy <=>
2021-05-10 01:21:51 +0530

    slots selection added

--- screens/Volunteer/Home/RenderActivity.js
+++ screens/Volunteer/Home/RenderActivity.js
@@ -7,13 +7,42 @@ import Icon from 'react-native-vector-icons/Ionicons';
 
 import { connect } from "react-redux";
 
+class RadioButton extends React.Component {
+    render() {
+        return (
+            <View style={[{
+                height: 24,
+                width: 24,
+                borderRadius: 12,
+                borderWidth: 2,
+                borderColor: '#000',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }, this.props.style]}>
+                {
+                    this.props.selected == 1 ?
+                        <View style={{
+                            height: 12,
+                            width: 12,
+                            borderRadius: 6,
+                            backgroundColor: '#000',
+                        }} />
+                        : null
+                }
+            </View>
+        );
+    }
+}
+
 class RenderActivity extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
             modalVisible: false,
-            showJoin: true
+            showJoin: true,
+            days: ["mon", "tue", "wed", "thur", "fri", "sat", "sun"],
+            slotsAvailable: []
         }
     }
 
@@ -22,24 +51,75 @@ class RenderActivity extends React.Component {
             volunteerId: this.props.volunteerId,
             activityId: this.props.item._id
         }).then(response => {
-            console.log(response)
             Alert.alert(
                 title = "You have been added to the activity successfully!!"
             );
             this.props.refreshCallback();
         })
             .catch(error => {
-                console.log(error)
                 Alert.alert(
                     title = "Something went wrong.. not able to join the activity :("
                 )
             })
     }
 
+    slotSelection(index, index1) {
+
+        const slots = this.state.slotsAvailable[index];
+        let x = [];
+        if (slots) {
+            x = slots.map((value, index2) => {
+                if (index2 == index1) {
+                    return {
+                        ...value,
+                        "selected": value.selected == 0 ? 1 : 0
+                    };
+                }
+                return value
+            })
+        }
+        let y = this.state.slotsAvailable;
+        y[index] = [...x];
+        this.setState({ slotsAvailable: [...y] });
+    }
+
+    renderEachSlot(slot, index) {
+        return (
+            <View key={index}>
+                <Text style={styles.valueHeader}>{this.state.days[index]}</Text>
+                {slot.map((item, index1) => {
+                    return (
+                        <TouchableOpacity key={index1} onPress={() => this.slotSelection(index, index1)}>
+                            <View style={{ paddingLeft: 10, flexDirection: 'row' }} >
+                                <View>
+                                    <RadioButton selected={item.selected} />
+                                </View>
+                                <Text>{item.start} - {item.end}</Text>
+                            </View>
+                        </TouchableOpacity>
+                    )
+                })}
+            </View>
+        )
+    }
+
+    onPressModal() {
+        this.setState({ modalVisible: true })
+        let slots = [];
+        this.props.item.slots.forEach(value0 => {
+            let slot = [];
+            value0.forEach(value1 => {
+                slot.push({ "start": value1.start, "end": value1.end, "selected": 0 });
+            })
+            slots.push(slot);
+        });
+        this.setState({ slotsAvailable: slots });
+    };
+
     render() {
         return (
             <View>
-                <TouchableWithoutFeedback onPress={() => this.setState({ modalVisible: true })}>
+                <TouchableWithoutFeedback onPress={() => this.onPressModal()}>
                     <View>
                         <Card>
                             <View style={styles.valueContainer}>
@@ -124,7 +204,8 @@ class RenderActivity extends React.Component {
                                 </Card>
                                 <Card>
                                     <View>
-                                        
+                                        {this.state.slotsAvailable.map((item, index) => this.renderEachSlot(item, index))}
+
                                     </View>
                                 </Card>
                             </View>
@@ -183,7 +264,9 @@ function mapStateToProps(state) {
 };
 
 function mapDispatchToProps(dispatch) {
-    return {};
+    return {
+
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(RenderActivity);
