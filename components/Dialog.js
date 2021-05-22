4f05bfbbee398388fba7d9eb9fe6ab62e93f5014
Marella Shiva Sai Teja <shivasai962@gmail.com>
2021-05-22 15:20:41 +0530

    Changed styling

--- components/Dialog.js
+++ components/Dialog.js
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
     dialogView: {
         zIndex: 1,
         position: 'absolute',
-        top: 0,
+        bottom: 0,
         right:0,
         left: 0,
         height: 40,
